cc87f6bf714b3c8dfe0f577ebb26301b4bf9a9c8
Anna <ann271101@gmail.com>
2021-11-29 14:36:59 +0200

    added delete user buttons

--- src/main/java/com/application/restaurant/registration/RegistrationService.java
+++ src/main/java/com/application/restaurant/registration/RegistrationService.java
@@ -34,7 +34,7 @@ public class RegistrationService {
                         UserRoles.ROLE_REGISTERED_USER
                 )
         );
-        String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
+        String link = "http://localhost:8080/confirm?token=" + token;
         emailSender.send(
                 request.getEmail(),
                 buildEmail(request.getFirstName(), link));
