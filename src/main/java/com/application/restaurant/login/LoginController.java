2a726490be24ad8e7d0977dca6a3fd827672fb57
Anna <ann271101@gmail.com>
2021-12-13 05:35:10 +0200

    rf

--- src/main/java/com/application/restaurant/login/LoginController.java
+++ src/main/java/com/application/restaurant/login/LoginController.java
@@ -1,13 +1,23 @@
 package com.application.restaurant.login;
 
+import com.application.restaurant.dao.UserRepository;
+import com.application.restaurant.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping
 public class LoginController {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping("/login")
     public String login(Model model) {
